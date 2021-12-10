fae5eb756b6d6f36513a588b839e5b6126892ddb
Jonathan Murmu <jonathanmurmu@Jonathans-MacBook-Air.local>
2021-12-10 18:01:09 +0530

    basic react app with next js and sign up page

---
+++ client/pages/banana.js
@@ -0,0 +1,6 @@
+const Banana = () => {
+    return <h1> banana2</h1>
+
+}
+
+export default Banana;
---
+++ client/pages/index.js
@@ -0,0 +1,6 @@
+const LandingPage = () => {
+    return <h1> Landing page</h1>
+
+}
+
+export default LandingPage;
