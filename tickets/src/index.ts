40ab8e95a849826773dd75cc66253f87f5ad2048
Jonathan Murmu <jonathanmurmu@Jonathans-MacBook-Air.local>
2021-12-16 15:49:38 +0530

    curd operation

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,17 +8,18 @@ const start = async () => {
         throw new Error('JWT_KEY must be defined!')
     }
     if(!process.env.MONGO_URI) {
-        throw new Error('JWT_KEY must be defined!')
+        throw new Error('MONGO_URI must be defined!')
     }
+
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('Connected to Auth Mongo DB!')
+        console.log('Connected to Tickets Mongo DB!')
     } catch(err) {
         console.log(err)
     }
 
     app.listen(3000, () => {
-        console.log('Listening on port 3000 - Auth Service!')
+        console.log('Listening on port 3000 - Tickets Service!')
     });
 }
 
---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,89 @@
+import { requireAuth } from '@ticketsjm92/common';
+import request from 'supertest';
+import { app } from '../../app';
+
+import { Ticket } from '../../models/ticket';
+
+it('has a route handler listening to /api/tickets for post request',async () => {
+    const response = await request(app)
+        .post('/api/tickets')
+        .send({});
+    expect(response.status).not.toEqual(404)
+});
+
+
+it('can only be accessed if the user is signed in',async () => {
+    await request(app)
+        .post('/api/tickets')
+        .send({})
+        .expect(401)
+});
+
+it('returns a status other than 401 if the user is sign in',async () => {
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin()).send({});
+    
+    console.log(response.status)
+    expect(response.status).not.toEqual(401);
+})
+
+
+it('return an error if an invalid title is provided',async () => {
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price: 10
+        })
+        .expect(400);
+    
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            price: 10
+        })
+        .expect(400);
+});
+
+
+it('return an error if an invalid price is provided',async () => {
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'SomeTitle',
+            price: -10
+        })
+        .expect(400);
+    
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'SomeTitle'
+        })
+        .expect(400);
+});
+
+it('creates a test with valid inputs',async () => {
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'title',
+            price: 20
+        })
+        .expect(201);
+    
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+
+    expect(tickets[0].price).toEqual(20);
+    expect(tickets[0].title).toEqual('title');
+});
