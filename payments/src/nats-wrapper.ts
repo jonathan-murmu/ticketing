c99eac0a6334028f830eef9f9283e5adf2f7b544
Jonathan Murmu <jonathanmurmu@Jonathans-MacBook-Air.local>
2022-02-24 21:12:22 +0530

    payments in progress

---
+++ payments/src/nats-wrapper.ts
@@ -0,0 +1,36 @@
+import nats, { Stan } from 'node-nats-streaming';
+                           
+
+
+class NatsWrapper {
+    private _client?: Stan;  // ? meaning might be undefiner for some period of time
+
+    get client() {
+        if (!this._client) {
+            throw new Error('Cannot access Nats client before connecting')
+        }
+        return this._client;
+    }
+
+    
+    connect(clusterId: string, clientId: string, url: string) {
+        this._client = nats.connect(clusterId, clientId, { url });
+
+        
+
+        return new Promise<void>( (resolve, reject) => {
+            this.client.on('connect', () => {
+                console.log('Connected to Nats');
+                resolve();
+            });
+            this.client.on('error', (err) => {
+                reject(err);
+            });
+        });
+        
+    }
+
+
+}
+
+export const natsWrapper = new NatsWrapper()
